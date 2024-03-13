ee8e0eb0e873021a63ea37c8743f1439016e2cc5
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-03-13 17:04:12 +0700

    fix: fix multimedia new api error

--- src/components/SwipeCard/SwipeCardItem.js
+++ src/components/SwipeCard/SwipeCardItem.js
@@ -9,33 +9,37 @@ const cx = classNames.bind(styles);
 function SwipeCardItem({ data, pageNumber, totalPage }) {
     return (
         <div className={cx('card-item-wrapper')}>
-            <div className="d-flex flex-column justify-content-between h-100">
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href={data?.url}
-                    className={cx('card-item-info')}
-                >
-                    <div className={cx('card-item-thumb')}>
-                        <Image src={data?.multimedia[0]?.url} alt="News image" />
-                    </div>
+            {
+                <div className="d-flex flex-column justify-content-between h-100">
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={data?.url}
+                        className={cx('card-item-info')}
+                    >
+                        <div className={cx('card-item-thumb')}>
+                            {data?.multimedia && data.multimedia.length > 0 && (
+                                <Image src={data?.multimedia[0]?.url} alt="News image" />
+                            )}
+                        </div>
 
-                    <p className={cx('card-item-heading')}>
-                        <span>
-                            <AiFillFire />
+                        <p className={cx('card-item-heading')}>
+                            <span>
+                                <AiFillFire />
+                            </span>
+                            Hot news
+                        </p>
+
+                        <p className={cx('card-item-desc')}>{data?.abstract || 'No Description'}</p>
+                    </a>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <p className={cx('card-item-section')}>{data?.section}</p>
+                        <span className={cx('card-item-page')}>
+                            {pageNumber}/{totalPage}
                         </span>
-                        Hot news
-                    </p>
-
-                    <p className={cx('card-item-desc')}>{data?.abstract || 'No Description'}</p>
-                </a>
-                <div className="d-flex justify-content-between align-items-center">
-                    <p className={cx('card-item-section')}>{data?.section}</p>
-                    <span className={cx('card-item-page')}>
-                        {pageNumber}/{totalPage}
-                    </span>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
